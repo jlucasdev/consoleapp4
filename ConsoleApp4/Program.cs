e144a7d63b07692077ad10e84020f6d479e7dccf
João Lucas <joaolucaspdsilva1@gmail.com>
2021-08-25 22:31:34 -0300

    5 commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        private static bool linha;
+
+        //PacelCase
+        //camelCase
+        //under_line
+
+        public static int CalcularSoma()
+        {
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+            return c;
+        }
+
+        public static void MostrarMensagemNaTela()
+        {
+            Console.WriteLine("ola pessoal");
+        }
+
+        public static void Tabuada(int numero)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(numero + " X " + i + " = " + (numero * i));
+            }
+            Console.WriteLine("=================");
+        }
+
+        private static void LerArquivo(string nomeArquivo)
+        {
+            using (StreamReader aquivo = File.OpenText(nomeArquivo))
+            {
+                string linha;
+                while ((linha = aquivo.ReadLine()) != null)
+                {
+                    Console.WriteLine(linha);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            LerArquivo(@"E:\arquivo\arq1.txt");
+        }
+    }
+}
